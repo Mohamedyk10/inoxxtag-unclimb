738542606221915c82a3ac072b9faf748f5e899f
RREEMMII <64311122+rreemmii-dev@users.noreply.github.com>
2025-04-05 15:51:01 +0200

    Ajout des piolets

--- inoxtag-unclimb/player.gd
+++ inoxtag-unclimb/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 const HORIZONTAL_SPEED = 500
-const JUMP_SPEED = -1500
+const VERTICAL_SPEED = 200
+const JUMP_INITIAL_SPEED = -1500
 
-const GRAVITY_G = 7500
+const GRAVITY_ACCELERATION = 7500
 
 var is_jumping = false
 var vertical_speed = 0
@@ -12,25 +13,33 @@ func _ready() -> void:
 	pass
 
 func _process(delta) -> void:
-	velocity = Vector2(0, vertical_speed)
-	var up = Input.is_action_pressed("JUMP")
-	var left = Input.is_action_pressed("LEFT")
-	var right = Input.is_action_pressed("RIGHT")
+	velocity = Vector2(0, 0)
 	if is_on_floor():
 		vertical_speed = 0
-	if up:
+	vertical_speed += GRAVITY_ACCELERATION * delta
+	if Input.is_action_pressed("JUMP"):
 		if is_jumping:
-			vertical_speed += 0.5 * GRAVITY_G * delta
+			vertical_speed -= 0.5 * GRAVITY_ACCELERATION * delta  # We remove 50% of gravity if jumping
 		else:
 			if is_on_floor():
 				is_jumping = true
-				vertical_speed = JUMP_SPEED
+				vertical_speed = JUMP_INITIAL_SPEED
 	else:
 		is_jumping = false
-		vertical_speed += GRAVITY_G * delta
-	velocity[1] = vertical_speed
-	if left:
+	var uses_ice_axe = false
+	if Input.is_action_pressed("LEFT"):
 		velocity[0] += -HORIZONTAL_SPEED
-	if right:
+		if is_on_wall():
+			uses_ice_axe = true
+	if Input.is_action_pressed("RIGHT"):
 		velocity[0] += HORIZONTAL_SPEED
+		if is_on_wall():
+			uses_ice_axe = true
+	if uses_ice_axe:
+		vertical_speed = 0
+	if Input.is_action_pressed("UP") and uses_ice_axe:
+		vertical_speed -= VERTICAL_SPEED
+	if Input.is_action_pressed("DOWN") and uses_ice_axe:
+		vertical_speed += VERTICAL_SPEED
+	velocity[1] = vertical_speed
 	move_and_slide()
