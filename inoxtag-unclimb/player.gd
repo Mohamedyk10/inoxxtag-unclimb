c1a8e70e4dea454ba354691eb5a39ca7a62da6ce
RREEMMII <64311122+rreemmii-dev@users.noreply.github.com>
2025-04-05 20:16:23 +0200

    Zipline added

--- inoxtag-unclimb/player.gd
+++ inoxtag-unclimb/player.gd
@@ -3,20 +3,43 @@ extends CharacterBody2D
 const HORIZONTAL_SPEED = 500
 const VERTICAL_SPEED = 200
 const JUMP_INITIAL_SPEED = -1500
+const ZIPLINE_SPEED = 1000
 
 const GRAVITY_ACCELERATION = 7500
 
 var is_jumping = false
 var vertical_speed = 0
 
-var is_in_zip_line_area = false
+var zip_line_coefs = null
+
 
 func _ready() -> void:
 	$Animation.play("stop")
 	set_floor_stop_on_slope_enabled(false)
 
+
+func f(x: float, coefs: Vector4) -> float:
+	var A = coefs[0]
+	var B = coefs[1]
+	var C = coefs[2]
+	var D = coefs[3]
+	return (D - B) / (C - A) * x + B - (D - B) / (C - A) * A
+
+
 func _process(delta) -> void:
 	velocity = Vector2(0, 0)
+	
+	if zip_line_coefs != null and not Input.is_action_pressed("JUMP"):
+		var x = global_position[0]
+		var y = global_position[1]
+		print("-------")
+		print(x, " ", y)
+		x = x + ZIPLINE_SPEED * delta
+		y = f(x, zip_line_coefs)
+		print(x, " ", y)
+		global_position = Vector2(x, y)
+		move_and_slide()
+		return
 
 	if is_on_floor():
 		vertical_speed = 0
@@ -32,12 +55,11 @@ func _process(delta) -> void:
 			uses_ice_axe = true
 	if uses_ice_axe:
 		vertical_speed = 0
-
 	if Input.is_action_pressed("JUMP"):
 		if is_jumping:
 			vertical_speed += 0.5 * GRAVITY_ACCELERATION * delta
 		else:
-			if is_on_floor() or is_on_wall():
+			if is_on_floor() or is_on_wall() or zip_line_coefs != null:
 				is_jumping = true
 				vertical_speed = JUMP_INITIAL_SPEED
 	else:
--- inoxtag-unclimb/zip_line.gd
+++ inoxtag-unclimb/zip_line.gd
@@ -1,9 +1,28 @@
 extends Area2D
 
+# Coordinates: ((a, b), (c, d))
+var coefs
+
+func _ready() -> void:
+	var RECT = $CollisionShape2D.shape.get_rect()
+	coefs = Vector4(RECT.position[0], RECT.position[1], RECT.end[0], RECT.end[1])
+
+func f(x: float, coefs: Vector4) -> float:
+	var A = coefs[0]
+	var B = coefs[1]
+	var C = coefs[2]
+	var D = coefs[3]
+	return (D - B) / (C - A) * x + B - (D - B) / (C - A) * A
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		body.is_in_zip_line_area = true
+		var x = body.global_position[0]
+		var y = f(x, coefs)
+		print(x, y)
+		body.global_position = Vector2(x, y)
+		body.move_and_slide()
+		body.zip_line_coefs = coefs
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
-		body.is_in_zip_line_area = false
+		body.zip_line_coefs = null
